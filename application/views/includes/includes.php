cea86c7481fa98d242e688a0abe830b0629d69e3
eMTy Codes <reymarktimkang24@gmail.com>
2024-04-11 20:42:09 +0800

    added local bootstrap

--- application/views/includes/includes.php
+++ application/views/includes/includes.php
@@ -1,5 +1,8 @@
-<link rel="stylesheet" href="<?php echo base_url() . "assets/css/mystyles.css" ?>">
+<link rel="stylesheet" href="<?php echo base_url() . "assets/css/custom/mystyles.css" ?>">
+<link rel="stylesheet" href="<?php echo base_url() . "assets/css/custom/header.css" ?>">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+<!--Backup incase of Internet issues-->
+<link rel="stylesheet" href="<?php echo base_url() . "assets/css/bootstrap/bootstrap.min.css" ?>">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.all.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
---
+++ application/views/includes/navigation.php
@@ -0,0 +1,8 @@
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="<?=base_url('assets/images/logo/rmt-logo-nobg.png')?>" alt="Logo" width="50" height="50" class="d-inline-block align-text-center">
+      RMT Apartment
+    </a>
+  </div>
+</nav>
