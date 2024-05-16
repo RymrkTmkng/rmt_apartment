b3d4b4a38c06724ee90316246ce70c61d3d71a01
eMTy Codes <reymarktimkang24@gmail.com>
2024-05-16 19:50:43 +0800

    updated db

---
+++ db/rmt_apartment_updated.sql
@@ -0,0 +1,306 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2024 at 12:42 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rmt_apartment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcement`
+--
+
+CREATE TABLE `announcement` (
+  `announcement_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `subject` varchar(20) NOT NULL,
+  `message` varchar(250) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `announcement`
+--
+
+INSERT INTO `announcement` (`announcement_id`, `user_id`, `subject`, `message`, `date_created`) VALUES
+(1, 0, 'Welcome', 'Greetings! We welcome you to out official website. Explore this website\'s features that will ease your home experience.', '2024-04-22 14:29:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `bill_id` int(10) NOT NULL,
+  `tenant_id` int(10) NOT NULL,
+  `billing_date` date NOT NULL,
+  `electric_kwh` varchar(10) NOT NULL,
+  `water_bill` varchar(10) NOT NULL,
+  `room_rent` varchar(10) NOT NULL,
+  `due_total` varchar(10) NOT NULL,
+  `status` varchar(3) NOT NULL COMMENT '0 - Pending, 1 - Unpaid, 2 - Paid, 3 - With Balance',
+  `invoice_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `concern`
+--
+
+CREATE TABLE `concern` (
+  `concern_id` int(10) NOT NULL,
+  `tenant_id` int(10) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `message` varchar(250) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `room_number` int(10) NOT NULL,
+  `floor` varchar(3) NOT NULL,
+  `room_dimension` varchar(50) NOT NULL,
+  `room_feature` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`room_number`, `floor`, `room_dimension`, `room_feature`, `description`) VALUES
+(1, '1st', '10x12sqft', 'Studio Type', 'Own CR and sink with exhaust fan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenant`
+--
+
+CREATE TABLE `tenant` (
+  `tenant_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `room_number` int(10) NOT NULL,
+  `starting_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tenant`
+--
+
+INSERT INTO `tenant` (`tenant_id`, `user_id`, `room_number`, `starting_date`) VALUES
+(1, 1, 1, '2022-01-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(10) NOT NULL,
+  `user_info_id` int(10) NOT NULL,
+  `role_id` int(10) NOT NULL COMMENT '1 - Admin, 2 - Tenant',
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_info_id`, `role_id`, `username`, `password`) VALUES
+(0, 0, 1, 'Admin', '$2y$10$bCCcYfV/KE10oluinxZCO.ZdHDAsSd/UKR.4dH3NpD7GeVqsNplfq'),
+(1, 1, 2, 'Reymark', '$2y$10$WBnJAAfkSDLABO2iNpHxue2HACTQKhf20g8AH0RE29T.V2k4A6LxW');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `user_info_id` int(10) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `age` varchar(2) NOT NULL,
+  `provincial_address` varchar(100) NOT NULL,
+  `occupation` varchar(20) DEFAULT NULL,
+  `registration_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`user_info_id`, `first_name`, `middle_name`, `last_name`, `email`, `birthdate`, `age`, `provincial_address`, `occupation`, `registration_date`) VALUES
+(0, 'Admin', 'Admin', 'Admin', '', '2022-01-01', '25', 'San Antonio, Linao', 'Admin', '2024-04-22 14:21:06'),
+(1, 'Reymark', 'Enot', 'Timkang', '', '2022-01-01', '25', 'San Antonio, Linao', 'Programmer', '2024-05-01 06:28:07'),
+(4, 'dwadaw', 'dawdwa', 'dwadwa', NULL, '0242-12-04', '', 'dwadawd', '2dwadwa', '2024-05-01 07:28:39'),
+(5, 'dwadaw', 'dawdwa', 'dwadwa', NULL, '0242-12-04', '', 'dwadawd', '2dwadwa', '2024-05-01 07:28:44');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD PRIMARY KEY (`announcement_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`bill_id`),
+  ADD KEY `ibfk_tenant_id` (`tenant_id`);
+
+--
+-- Indexes for table `concern`
+--
+ALTER TABLE `concern`
+  ADD PRIMARY KEY (`concern_id`),
+  ADD KEY `tenant_id` (`tenant_id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`room_number`);
+
+--
+-- Indexes for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD PRIMARY KEY (`tenant_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `room_number` (`room_number`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_info_id` (`user_info_id`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`user_info_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `announcement`
+--
+ALTER TABLE `announcement`
+  MODIFY `announcement_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `bill_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `concern`
+--
+ALTER TABLE `concern`
+  MODIFY `concern_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `room_number` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tenant`
+--
+ALTER TABLE `tenant`
+  MODIFY `tenant_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `user_info_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD CONSTRAINT `announcement_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `bill`
+--
+ALTER TABLE `bill`
+  ADD CONSTRAINT `ibfk_tenant_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`tenant_id`);
+
+--
+-- Constraints for table `concern`
+--
+ALTER TABLE `concern`
+  ADD CONSTRAINT `concern_ibfk_1` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`tenant_id`);
+
+--
+-- Constraints for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD CONSTRAINT `tenant_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `tenant_ibfk_2` FOREIGN KEY (`room_number`) REFERENCES `room` (`room_number`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`user_info_id`) REFERENCES `user_info` (`user_info_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
